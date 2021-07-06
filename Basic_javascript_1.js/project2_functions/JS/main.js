b4060189de5d776d84af13eafb7c006f82866cb3
salsandoval <76705210+salsandoval@users.noreply.github.com>
2021-07-06 15:16:18 -0600

    changed var on on main js

--- Basic_javascript_1.js/project2_functions/JS/main.js
+++ Basic_javascript_1.js/project2_functions/JS/main.js
@@ -1,11 +1,11 @@
 function WARNING_DO_NOT_CLICK() {// To change color for on click button
-    var str = "This text is red!";
+    var str = "I SAID DO NOT CLICK!";
     var result = str.fontcolor("red");
     document.getElementById("Red_Text").innerHTML = result;
 }
 
 function myFunction() { // used += to concatenate str 
-    var sentence = "I am learning";
-    sentence += " A lot from this book!";
+    var sentence = "I SAID DO NOT CLICK";
+    sentence += " YOU REBEL!";
     document.getElementById("concatenate") .innerHTML = sentence;
 }
