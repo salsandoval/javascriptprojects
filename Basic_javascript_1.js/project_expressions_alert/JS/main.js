e131e06acf55e0c29b3d6c9706e91d80e153308c
salsandoval <76705210+salsandoval@users.noreply.github.com>
2021-07-06 10:30:53 -0600

    Added to basic javascript folder

--- Basic_javascript_1.js/project_expressions_alert/JS/main.js
+++ Basic_javascript_1.js/project_expressions_alert/JS/main.js
@@ -1,1 +1,24 @@
-document.write('Hello, World 123!');
+
+window.alert("I have always liked to play drums, \"i'll always like to play instruments!\
+\"Drums are life\"");// I added a window alert 
+
+document.write("\Do or do not there is no try,"
++ "- YODA.");
+
+var B = "YODA" + "S";
+document.write(B)
+
+var Family = " The sandovals" , Dad = "Sal" , Mom = "Steph" , Son1 = "Isaak" , Son2 = "Makai" , Daughter1 = "Maddie" , 
+Daughter2 = "Stella" , Daughter3 = "Hazel";
+
+var Family = "Thesandovals";
+var Family = Family.fontcolor("blue");
+document.write(Family);
+
+var Dad = "Sal";// assigning a variable a name 
+var Dad = Dad.fontcolor("blue");// giving color to variable
+document.write(Mom);
+
+
+6 + 6
+document.write(6+6);
