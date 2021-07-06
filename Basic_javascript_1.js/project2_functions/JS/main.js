68341d810346dab22130096ee772d2c45dffa7e3
salsandoval <76705210+salsandoval@users.noreply.github.com>
2021-07-06 14:47:44 -0600

    added comments and fixed JS link

--- Basic_javascript_1.js/project2_functions/JS/main.js
+++ Basic_javascript_1.js/project2_functions/JS/main.js
@@ -1,10 +1,10 @@
-function WARNING_DO_NOT_CLICK() {
+function WARNING_DO_NOT_CLICK() {// To change color for on click button
     var str = "This text is red!";
     var result = str.fontcolor("red");
     document.getElementById("Red_Text").innerHTML = result;
 }
 
-function myFunction() {
+function myFunction() { // used += to concatenate str 
     var sentence = "I am learning";
     sentence += " A lot from this book!";
     document.getElementById("concatenate") .innerHTML = sentence;
